22bca9935270245b120fc553f0c7011568bd5abb
POOJA-PC\POOJA <pooja130192@gmail.com>
2018-01-11 00:59:10 +0530

    itemdetailactivity

--- app/src/main/java/com/example/pooja/myappdemonew/view/ItemDetailActivity.java
+++ app/src/main/java/com/example/pooja/myappdemonew/view/ItemDetailActivity.java
@@ -2,8 +2,13 @@ package com.example.pooja.myappdemonew.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.pooja.myappdemonew.R;
 
@@ -13,10 +18,49 @@ import com.example.pooja.myappdemonew.R;
 
 public class ItemDetailActivity extends AppCompatActivity{
     Toolbar toolbar;
+    CoordinatorLayout maincontent;
+    AppBarLayout appbar;
+    CollapsingToolbarLayout collapsing_toolbar;
+    ImageView header, imgUser;
+    TextView txt_item_name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
-        toolbar=(Toolbar)findViewById(R.id.prof_toolbar);
+
+        attachView();
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsing_toolbar.setTitle(txt_item_name.getText().toString());
+                    isShow = true;
+                } else if (isShow) {
+                    collapsing_toolbar.setTitle("");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+    private void attachView() {
+        toolbar=(Toolbar)findViewById(R.id.item_detail_toolbar);
+        maincontent = (CoordinatorLayout) findViewById(R.id.maincontent);
+        header = (ImageView) findViewById(R.id.header);
+        imgUser = (ImageView) findViewById(R.id.imgUser);
+        collapsing_toolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        appbar = (AppBarLayout) findViewById(R.id.appbar);
+        txt_item_name=(TextView)findViewById(R.id.txt_item_name);
     }
 }
